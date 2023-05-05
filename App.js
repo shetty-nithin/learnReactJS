d8dcc717bc205879852495d830fa8b05b3d95b2e
shetty-nithin <shettynithin744@gmail.com>
2023-05-05 23:06:20 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,74 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//Example 1:
+
+/* <h3 id="heading3">"Hello React.js"</h3> */
+/*
+const reactHeading = React.createElement("h3", {id: "heading3"}, "Hello React.js");
+console.log(reactHeading);// this returns an object not the html tag
+reactRoot.render(reactHeading);
+*/
+
+
+
+
+// Example 2: Signle Child
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>"Im h1 tag"</h1>
+    </div>
+</div>
+*/
+
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         React.createElement(
+//             "h1",
+//             {},
+//             "Im h1 tag"
+//         )
+//     )
+// )
+// console.log(parent);
+// root.render(parent);
+
+//Note: => "parent" is React Element not a tag.
+//      => react element becomes HTML object which browser understands.
+
+
+
+
+
+
+// Example 3: Multiple children
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>"Im h1 tag : 1"</h1>
+        <h1>"Im h1 tag : 2"</h1>
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        [
+            React.createElement("h1",{},"Im h1 tag : 1"),
+            React.createElement("h1", {}, "Im h1 tag : 2")
+        ]
+    )
+)
+console.log(parent);
+root.render(parent);
+
+//Note: => "parent" is React Element not a tag.
+//      => react element becomes HTML object which browser understands.
